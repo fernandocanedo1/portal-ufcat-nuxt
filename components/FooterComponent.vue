d5942eb41746317a3511bbcce31303ef4b6539bf
fernandocanedo1 <fern.luck@gmail.com>
2022-07-11 15:28:08 -0500

    First Commit

---
+++ components/FooterComponent.vue
@@ -0,0 +1,124 @@
+<template>
+    <footer class="br-footer">
+        <div class="container-lg">
+            <div class="logo">
+                <img src="http://200.18.165.172:8080/ufcat/@@site-logo/UFCAT_-_Identidade_Visual_Completa60.png"
+                    alt="Imagem" />
+            </div>
+            <div class="br-list horizontal" data-toggle="data-toggle" data-sub="data-sub">
+                <div class="col-2"><a class="br-item header" href="javascript:void(0)">
+                        <div class="content text-down-01 text-bold text-uppercase">NOTÍCIAS</div>
+                        <div class="support"><i class="fas fa-angle-down" aria-hidden="true"></i>
+                        </div>
+                    </a>
+                    <div class="br-list"><span class="br-divider d-md-none"></span><a class="br-item"
+                            href="javascript:void(0)">
+                            <div class="content">Adipisicing culpa et ad consequat</div>
+                        </a><a class="br-item" href="javascript:void(0)">
+                            <div class="content">Ad deserunt nostrud</div>
+                        </a><a class="br-item" href="javascript:void(0)">
+                            <div class="content">Ex qui laborum consectetur aute commodo</div>
+                        </a><span class="br-divider d-md-none"></span>
+                    </div>
+                </div>
+                <div class="col-2"><a class="br-item header" href="javascript:void(0)">
+                        <div class="content text-down-01 text-bold text-uppercase">EVENTOS</div>
+                        <div class="support"><i class="fas fa-angle-down" aria-hidden="true"></i>
+                        </div>
+                    </a>
+                    <div class="br-list"><span class="br-divider d-md-none"></span><a class="br-item"
+                            href="javascript:void(0)">
+                            <div class="content">Duis incididunt consectetur</div>
+                        </a><a class="br-item" href="javascript:void(0)">
+                            <div class="content">Ex qui laborum consectetur aute commodo</div>
+                        </a><a class="br-item" href="javascript:void(0)">
+                            <div class="content">Nulla occaecat eiusmod</div>
+                        </a><a class="br-item" href="javascript:void(0)">
+                            <div class="content">Deserunt</div>
+                        </a><span class="br-divider d-md-none"></span>
+                    </div>
+                </div>
+                <div class="col-2"><a class="br-item header" href="javascript:void(0)">
+                        <div class="content text-down-01 text-bold text-uppercase">USUÁRIOS</div>
+                        <div class="support"><i class="fas fa-angle-down" aria-hidden="true"></i>
+                        </div>
+                    </a>
+                    <div class="br-list"><span class="br-divider d-md-none"></span><a class="br-item"
+                            href="javascript:void(0)">
+                            <div class="content">Ex qui laborum consectetur aute commodo</div>
+                        </a><a class="br-item" href="javascript:void(0)">
+                            <div class="content">Duis incididunt consectetur</div>
+                        </a><a class="br-item" href="javascript:void(0)">
+                            <div class="content">Adipisicing culpa et ad consequat</div>
+                        </a><span class="br-divider d-md-none"></span>
+                    </div>
+                </div>
+                <div class="col-2"><a class="br-item header" href="javascript:void(0)">
+                        <div class="content text-down-01 text-bold text-uppercase">SETI</div>
+                        <div class="support"><i class="fas fa-angle-down" aria-hidden="true"></i>
+                        </div>
+                    </a>
+                    <div class="br-list"><span class="br-divider d-md-none"></span><a class="br-item"
+                            href="javascript:void(0)">
+                            <div class="content">Adipisicing culpa et ad consequat</div>
+                        </a><a class="br-item" href="javascript:void(0)">
+                            <div class="content">Ex qui laborum consectetur aute commodo</div>
+                        </a><a class="br-item" href="javascript:void(0)">
+                            <div class="content">Adipisicing culpa et ad consequat</div>
+                        </a><span class="br-divider d-md-none"></span>
+                    </div>
+                </div>
+                <div class="col-2"><a class="br-item header" href="javascript:void(0)">
+                        <div class="content text-down-01 text-bold text-uppercase">AJUDA</div>
+                        <div class="support"><i class="fas fa-angle-down" aria-hidden="true"></i>
+                        </div>
+                    </a>
+                    <div class="br-list"><span class="br-divider d-md-none"></span><a class="br-item"
+                            href="javascript:void(0)">
+                            <div class="content">Adipisicing culpa et ad consequat</div>
+                        </a><a class="br-item" href="javascript:void(0)">
+                            <div class="content">Est ex deserunt</div>
+                        </a><a class="br-item" href="javascript:void(0)">
+                            <div class="content">Nulla occaecat eiusmod</div>
+                        </a><span class="br-divider d-md-none"></span>
+                    </div>
+                </div>
+                <div class="col-2"><a class="br-item header" href="javascript:void(0)">
+                        <div class="content text-down-01 text-bold text-uppercase">Categoria 6</div>
+                        <div class="support"><i class="fas fa-angle-down" aria-hidden="true"></i>
+                        </div>
+                    </a>
+                    <div class="br-list"><span class="br-divider d-md-none"></span><a class="br-item"
+                            href="javascript:void(0)">
+                            <div class="content">Est ex deserunt</div>
+                        </a><a class="br-item" href="javascript:void(0)">
+                            <div class="content">Nulla occaecat eiusmod</div>
+                        </a><a class="br-item" href="javascript:void(0)">
+                            <div class="content">Deserunt</div>
+                        </a><span class="br-divider d-md-none"></span>
+                    </div>
+                </div>
+            </div>
+            <div class="d-none d-sm-block">
+                <div class="row align-items-end justify-content-between py-5">
+                    <div class="col social-network">
+                        <p class="text-up-01 text-extra-bold text-uppercase">Redes Sociais</p><a class="mr-3"
+                            href="javascript:void(0);"></a>
+                    </div>
+                    <div class="col assigns text-right"><img class="ml-4"
+                            src="https://cdn.dsgovserprodesign.estaleiro.serpro.gov.br/design-system/images/logo-assign-negative.png"
+                            alt="Imagem" /><img class="ml-4"
+                            src="https://cdn.dsgovserprodesign.estaleiro.serpro.gov.br/design-system/images/logo-assign-negative.png"
+                            alt="Imagem" />
+                    </div>
+                </div>
+            </div>
+        </div><span class="br-divider my-3"></span>
+        <div class="container-lg">
+            <div class="info">
+                <div class="text-down-01 text-medium pb-3">Texto destinado a exibição de informações relacionadas
+                    à&nbsp;<strong>licença de uso.</strong></div>
+            </div>
+        </div>
+    </footer>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="template-base">
+    <HeaderComponent />
+    <ContentComponent />
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from '../components/HeaderComponent.vue';
+import FooterComponent from '../components/FooterComponent.vue';
+import ContentComponent from '../components/ContentComponent.vue';
+export default {
+  name: "IndexPage",
+  components: { HeaderComponent, FooterComponent, ContentComponent }
+}
+</script>
+
+<style>
+@import "https://cdn.dsgovserprodesign.estaleiro.serpro.gov.br/design-system/fonts/rawline/css/rawline.css";
+@import "https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700,800,900&amp;display=swap";
+@import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css";
+
+</style>
