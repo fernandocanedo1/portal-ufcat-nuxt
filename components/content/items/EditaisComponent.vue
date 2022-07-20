1c733d67096c3ee3225483ae832b38e74785b6c1
fernandocanedo1 <fern.luck@gmail.com>
2022-07-20 16:48:29 -0500

    components/css

--- components/content/items/EditaisComponent.vue
+++ components/content/items/EditaisComponent.vue
@@ -1,16 +1,16 @@
 <template>
+    <!--
     <div class="container">
         <div class="row">
-            <div class="col-lg-4">
+            <div class="col-md-6 col-lg-4">
                 <div class="card card-margin">
                     <div class="card-body pt-0">
-                        <div class="widget-49">
+                        <div class="widget-48">
                             <div class="widget-49-title-wrapper">
-                                <div class="widget-49-date-primary">
-                                    <span class="widget-49-date-day">09</span>
-                                    <span class="widget-49-date-month">apr</span>
+                                <div class="widget-image">
+                                    <img src="https://files.cercomp.ufg.br/weby/up/519/m/Logo_-_Athenas_CEI.png?1459257795">
                                 </div>
-                                <div class="widget-49-meeting-info">
+                                <div class="widget-49-meeting-info edital">
                                     <span class="widget-49-pro-title">VI Congresso Inovação e Metodologias no Ensino
                                         Superior e Tecnológico prorroga inscrições de trabalhos</span>
                                     <span class="widget-49-meeting-time">12:00 to 13.30 Hrs</span>
@@ -20,16 +20,13 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4">
+            <div class="col-md-6 col-lg-4">
                 <div class="card card-margin">
 
                     <div class="card-body pt-0">
                         <div class="widget-49">
                             <div class="widget-49-title-wrapper">
-                                <div class="widget-49-date-primary">
-                                    <span class="widget-49-date-day">13</span>
-                                    <span class="widget-49-date-month">apr</span>
-                                </div>
+                              
                                 <div class="widget-49-meeting-info">
                                     <span class="widget-49-pro-title">IV Simpósio de Engenharia de Produção</span>
                                     <span class="widget-49-meeting-time">12:00 to 13.30 Hrs</span>
@@ -41,16 +38,13 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4">
+            <div class="col-md-6 col-lg-4">
                 <div class="card card-margin">
 
                     <div class="card-body pt-0">
                         <div class="widget-49">
                             <div class="widget-49-title-wrapper">
-                                <div class="widget-49-date-success">
-                                    <span class="widget-49-date-day">22</span>
-                                    <span class="widget-49-date-month">apr</span>
-                                </div>
+                          
                                 <div class="widget-49-meeting-info">
                                     <span class="widget-49-pro-title">XXI Encontro Nacional de Didática e Práticas de
                                         Ensino (ENDIPE)</span>
@@ -63,5 +57,51 @@
             </div>
         </div>
     </div>
+-->
+    <div class="edital-cards">
+        <div class="row mb-md-2">
+            <div class="col-md-6 col-lg-4">
+                <div class="br-card  ">
+                    <a href="#" class="position-relative">
+                        <img src="https://files.cercomp.ufg.br/weby/up/519/m/Logo_-_Athenas_CEI.png?1459257795"
+                            class="card-img-top" alt="image"> </a>
+                    <div class="tile-subtitle">
+                        <a href="#">
+                            <h5 class="font-weight-normal">Retorno das atividades do TRAVESSIA: Grupo de Acolhimento de
+                                estudantes de graduação</h5>
+                        </a>
 
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="br-card  ">
+                    <a href="#" class="position-relative">
+                        <img src="http://200.18.165.172:8080/ufcat/secretarias/seti/f91ca042-e4c4-4df5-9b53-451efebb74b1.jpeg/@@images/2e2c9978-8ecf-4f6e-a701-09430a824b93.jpeg"
+                            class="card-img-top" alt="image"> </a>
+                    <div class="tile-subtitle">
+                        <a href="#">
+                            <h5 class="font-weight-normal">Retorno das atividades do TRAVESSIA: Grupo de Acolhimento de
+                                estudantes de graduação</h5>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="br-card  ">
+                    <a href="#" class="position-relative">
+                        <img src="http://200.18.165.172:8080/ufcat/secretarias/seti/f91ca042-e4c4-4df5-9b53-451efebb74b1.jpeg/@@images/2e2c9978-8ecf-4f6e-a701-09430a824b93.jpeg"
+                            class="card-img-top" alt="image"> </a>
+                    <div class="tile-subtitle">
+                        <a href="#">
+                            <h5 class="font-weight-normal">Secretaria de Tecnologia da Informação comunica reforma de
+                                datacenter e paradas de internet</h5>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
